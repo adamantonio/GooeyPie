81a0581fd3c7d9ebdd53b2412c6a8a2700417298
Adam Antonio <adamantonio@gmail.com>
2026-01-18 11:59:21 +1100

    Improved button test app

--- examples/widgets/buttons_test.py
+++ examples/widgets/buttons_test.py
@@ -1,51 +1,55 @@
 import gooeypie as gp
+import time
 
 
-def disable(event):
-    event.widget.disabled = True
+def button_press(event):
+    simple_status.text = f"Button Pressed at {time.strftime('%H:%M:%S')}"
+
+def toggle_state(event):
+    disabled_btn.disabled = not disabled_btn.disabled
+
+def button_status(event):
+    disabled_status.text = f"Button Pressed at {time.strftime('%H:%M:%S')}"
 
 def change_text(event):
-    event.widget.text = "Text Changed"
+    change_text_btn.text = "Text Changed"
+    change_text_status.text = f"Button text is '{change_text_btn.text}'"
+
 
 app = gp.GooeyPieApp("Buttons Test")
 # app.theme = "light"
 
-button_standard = gp.Button("Standard button (click to disable)", disable)
 
-button_disabled = gp.Button("Disabled button", None)
-button_disabled.disabled = True
-button_disabled.style.disabled_text_color = "darkgray"
+# Test the press event
+simple_lbl = gp.Label("Simple Button")
+simple_btn = gp.Button("Click Me", button_press)
+simple_status = gp.Label("Status")
 
-button_outline = gp.Button("Outline button", change_text)
-button_outline.style.border_color = "violet"
-button_outline.style.border_width = 2
+app.add(simple_lbl, 1, 1)
+app.add(simple_btn, 3, 1)
+app.add(simple_status, 4, 1)
 
-button_square = gp.Button("Square button", None)
-button_square.style.corner_radius = 0
 
-button_colored = gp.Button("Colored button", None)
-button_colored.style.bg_color = ("green", "tomato")
-button_colored.style.hover_bg_color = ("darkgreen", "darkred")
-button_colored.style.text_color = "white"
+# Test the disabled property
+disabled_lbl = gp.Label("Disabled Button")
+disable_action_btn = gp.Button("Toggle state", toggle_state)
+disabled_btn = gp.Button("Try to click Me", button_status)
+disabled_status = gp.Label("Status")
 
-button_pill = gp.Button("Pill button", None)
-button_pill.style.corner_radius = 20
+app.add(disabled_lbl, 1, 2)
+app.add(disable_action_btn, 2, 2)
+app.add(disabled_btn, 3, 2)
+app.add(disabled_status, 4, 2)
 
-button_spacious = gp.Button("Spacious button", None)
-button_spacious.style.padding = 20
 
-button_large = gp.Button("Large button", None)
-button_large.style.text_color = "white"
-button_large.width = 200
-button_large.height = 100
+# Test the text property
+change_text_lbl = gp.Label("Change Text Button")
+change_text_btn = gp.Button("Change Text", change_text)
+change_text_status = gp.Label("Status")
 
-app.add(button_standard, 1, 1)
-app.add(button_disabled, 1, 2)
-app.add(button_outline, 1, 3)
-app.add(button_square, 1, 4)
-app.add(button_colored, 1, 5)
-app.add(button_pill, 1, 6)
-app.add(button_spacious, 1, 7)
-app.add(button_large, 1, 8)
+app.add(change_text_lbl, 1, 3)
+app.add(change_text_btn, 3, 3)
+app.add(change_text_status, 4, 3)
+
 
 app.run()
